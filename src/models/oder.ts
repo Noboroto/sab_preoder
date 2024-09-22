60dc7213cea05e4ea0aebff2af4294600c5b6979
David0403 <thanhtuvo135@gmail.com>
2024-09-22 18:29:20 +0700

    Add Zod schema

--- src/models/oder.ts
+++ src/models/oder.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export interface Order {
 	id: string;
 	dayAndTime: Date;
@@ -11,3 +13,17 @@ export interface Order {
 	lanyard3Amount: number;
 	totalMoney: number;
 }
+
+export const OrderSchema = z.object({
+	id: z.string().optional(),
+	dayAndTime: z.date(),
+	email: z.string(),
+	phone: z.string(),
+	name: z.string(),
+	blackHolderAmount: z.number(),
+	grayHolderAmount: z.number(),
+	lanyard1Amount: z.number(),
+	lanyard2Amount: z.number(),
+	lanyard3Amount: z.number(),
+	totalMoney: z.number(),
+});
