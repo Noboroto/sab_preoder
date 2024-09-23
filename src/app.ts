61980b840587d8f5cc1db087aea8de2ebfbbedaa
David0403 <thanhtuvo135@gmail.com>
2024-09-23 07:10:32 +0700

    Revert "Update app.ts"
    This reverts commit 7fc6f54ea131c44a83c7672ef05d9a41998139fd.

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,22 @@ dotenv.config();
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+			imgSrc: ["'self'", 'data:', 'https://img.vietqr.io'],
+		},
+	})
+); 
 
+const corsOptions = {
+	origin: '*', // Allow all domains
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+	allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', // Allowed headers
+	credentials: true, // Allow cookies to be sent (if needed)
+};
+app.use(cors(corsOptions));
 app.use(express.json()); // Add this line to enable JSON parsing in the request body
 app.use('/order', orderRoutes); 
 app.use('/storage', storageRoutes); 
