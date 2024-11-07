93becb059b268d78ac58ae46b93721671fb845b4
David0403 <thanhtuvo135@gmail.com>
2024-11-07 19:09:43 +0700

    Update prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
-import express, { Request, Response } from 'express';
-import transactionRoutes from './routes/transaction';
+import express, { Request, Response } from "express";
+import transactionRoutes from "./routes/transaction";
 import path from "path";
 
 dotenv.config();
@@ -13,33 +13,34 @@ const app = express();
 app.use(express.json()); // Add this line to enable JSON parsing in the request body
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/transaction', transactionRoutes); 
-if (DOMAIN.includes('preorder')) {
-	console.log('Using public folder');
-	app.use(express.static(path.join(path.dirname(__dirname), 'public')));
-}
-else {
-	console.log('Using public_offline folder');
-	app.use(express.static(path.join(path.dirname(__dirname), 'public_offline')));
+app.use("/transaction", transactionRoutes);
+if (DOMAIN.includes("preorder")) {
+  console.log("Using public folder");
+  app.use(express.static(path.join(path.dirname(__dirname), "public")));
+} else {
+  console.log("Using public_offline folder");
+  app.use(express.static(path.join(path.dirname(__dirname), "public_offline")));
 }
 
-app.get('/', (req: Request, res: Response) => {
-	console.log('GET /');
-	if (DOMAIN.includes('localhost')) {
-		console.info('DOMAIN includes localhost');
-		res.sendFile(path.join(path.dirname(__dirname), 'public_offline', 'index.html'));
-	}
-	else if (DOMAIN.includes('preorder')) {
-		console.info('DOMAIN includes preorder');
-		res.sendFile(path.join(path.dirname(__dirname), 'public', 'index.html'));
-	}
-	else {
-		console.info('DOMAIN includes production');
-		res.sendFile(path.join(path.dirname(__dirname), 'public_offline', 'index.html'));
-	}
+app.get("/", (req: Request, res: Response) => {
+  console.log("GET /");
+  if (DOMAIN.includes("localhost")) {
+    console.info("DOMAIN includes localhost");
+    res.sendFile(
+      path.join(path.dirname(__dirname), "public_offline", "index.html")
+    );
+  } else if (DOMAIN.includes("preorder")) {
+    console.info("DOMAIN includes preorder");
+    res.sendFile(path.join(path.dirname(__dirname), "public", "index.html"));
+  } else {
+    console.info("DOMAIN includes production");
+    res.sendFile(
+      path.join(path.dirname(__dirname), "public_offline", "index.html")
+    );
+  }
 });
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT} - http://localhost:${PORT}`);
-	console.log("DOMAIN:", DOMAIN);
+  console.log(`Listening on port ${PORT} - http://localhost:${PORT}`);
+  console.log("DOMAIN:", DOMAIN);
 });
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,32 +1,35 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
 
-export const sendEmail = async (recipient: string, replacements: Map<string, string>) => {
-	const transporter = nodemailer.createTransport({
-		service: "gmail",
-		host: "smtp.gmail.com",
-		port: 587,
-		secure: false,
-		auth: {
-			user: process.env.EMAIL,
-			pass: process.env.PASSWORD,
-		},
-	});
+export const sendEmail = async (
+  recipient: string,
+  replacements: Map<string, string>
+) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-	const subject = "[SAB] Xác nhận đặt trước vật phẩm SAB";
-	let text = fs.readFileSync("src/utils/email.txt", "utf8");
-	let html = fs.readFileSync("src/utils/email.html", "utf8");
+  const subject = "[SAB] Xác nhận đặt trước vật phẩm SAB";
+  let text = fs.readFileSync("src/utils/email.txt", "utf8");
+  let html = fs.readFileSync("src/utils/email.html", "utf8");
 
-	replacements.forEach((value, key) => {
-		text = text.replaceAll(`{{${key}}}`, value);
-		html = html.replaceAll(`{{${key}}}`, value);
-	});
+  replacements.forEach((value, key) => {
+    text = text.replaceAll(`{{${key}}}`, value);
+    html = html.replaceAll(`{{${key}}}`, value);
+  });
 
-	return await transporter.sendMail({
-		from: process.env.EMAIL,
-		to: recipient,
-		subject: subject,
-		text: text,
-		html: html
-	});
-}
+  return await transporter.sendMail({
+    from: process.env.EMAIL,
+    to: recipient,
+    subject: subject,
+    text: text,
+    html: html,
+  });
+};
